a1b16b276aee4e413dd9b68508b77a2845a4c914
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2019-12-18 19:17:36 -0500

    MVP

--- users/user-router.js
+++ users/user-router.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
 
 const db = require("./user-model");
+const restricted = require("../auth/restricted-middleware");
 
 // EndPoints: /api/users
+router.get("/", restricted, (req, res) => {
+  db.find()
+    .then(users => {
+      res.json({ users });
+    })
+    .catch(err => res.send(err));
+});
 
 // Middleware
 
